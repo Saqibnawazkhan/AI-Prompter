07310f1db1e14e1980ab6d33dc60d1a2dc266d59
Saqib nawaz Khan <67519982+Saqibnawazkhan@users.noreply.github.com>
2026-01-06 07:31:10 +0500

    Change 49: Add hooks barrel export - Created index.ts for all custom hooks - Organized exports for cleaner imports - Includes all utility hooks

---
+++ hooks/index.ts
@@ -0,0 +1,13 @@
+// Barrel export for hooks
+export { useConfetti } from './useConfetti';
+export { useHistory, type HistoryItem } from './useHistory';
+export { useKeyboardShortcuts, type Shortcut } from './useKeyboardShortcuts';
+export { useFocusTrap } from './useFocusTrap';
+export {
+  useMediaQuery,
+  useIsMobile,
+  useIsTablet,
+  useIsDesktop,
+  useIsDarkMode,
+  useIsReducedMotion,
+} from './useMediaQuery';
