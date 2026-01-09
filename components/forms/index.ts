da0c47e4ae6064f90b26f102b4f78f78ab1ff047
Saqib nawaz Khan <67519982+Saqibnawazkhan@users.noreply.github.com>
2026-01-09 23:09:30 +0500

    Change 21/50: Update forms barrel export - Added EducationPromptForm export - Added CreativePromptForm export

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -3,3 +3,5 @@ export { default as ImagePromptForm } from './ImagePromptForm';
 export { default as WritingPromptForm } from './WritingPromptForm';
 export { default as MarketingPromptForm } from './MarketingPromptForm';
 export { default as BusinessPromptForm } from './BusinessPromptForm';
+export { default as EducationPromptForm } from './EducationPromptForm';
+export { default as CreativePromptForm } from './CreativePromptForm';
