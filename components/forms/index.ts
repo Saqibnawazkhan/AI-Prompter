f65d1d3fcf7bba610c60ede3b261f0531f76e21f
Saqib nawaz Khan <67519982+Saqibnawazkhan@users.noreply.github.com>
2026-01-09 22:56:00 +0500

    Change 13/50: Create forms barrel export - Export all form components from single file - Cleaner imports for form components

---
+++ components/forms/index.ts
@@ -0,0 +1,5 @@
+// Forms barrel export
+export { default as ImagePromptForm } from './ImagePromptForm';
+export { default as WritingPromptForm } from './WritingPromptForm';
+export { default as MarketingPromptForm } from './MarketingPromptForm';
+export { default as BusinessPromptForm } from './BusinessPromptForm';
